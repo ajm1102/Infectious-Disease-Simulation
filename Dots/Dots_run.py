15bd13abc5ec5e9fd77d72158fc651ffb2641daa
ajm1102 <ajm1102.moore@gmail.com>
2022-01-13 16:55:07 +0000

    Multiple dots

--- Dots/Dots_run.py
+++ Dots/Dots_run.py
@@ -1,62 +1,64 @@
-import random
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import matplotlib.pyplot as plt
 from Dots_Objects import *
 
 def CheckBoundaryLimits(x, y):
-    if x > 100:
-        x = 100
+    if x > Boundary.x:
+        x = Boundary.x
         person.vx = person.vx * -1
-    if y > 100:
-        y = 100
+    if y > Boundary.y:
+        y = Boundary.y
         person.vy = person.vy * -1
-    if x < -100:
-        x = -100
+    if x < -Boundary.x:
+        x = -Boundary.x
         person.vx = person.vx * -1
-    if y < -100:
-        y = -100
+    if y < -Boundary.y:
+        y = -Boundary.y
         person.vy = person.vy * -1
     return x, y
 
 
-simlength = 7000 #
-Boundary.x = 100
+people = []
+num_people = 25
+simlength = 7000
+Boundary.x = 50
 Boundary.y = 100
-person = Dot()
 
-# Initial x and y position
-person.x = np.random.randint((-1 * Boundary.x), Boundary.y, 1)
-person.y = np.random.randint((-1 * Boundary.x), Boundary.y, 1)
+for j in range(num_people):
+    person = Dot()
+    # Initial x and y position
+    person.x = np.random.randint((-1 * Boundary.x), Boundary.y, 1)
+    person.y = np.random.randint((-1 * Boundary.x), Boundary.y, 1)
+    # Initial x and y velocity
+    person.vx, person.vy = np.random.uniform(-2, 2, 1), np.random.uniform(-2, 2, 1)
 
-# Initial x and y velocity
-person.vx, person.vy = np.random.uniform(-6, 6, 1), np.random.uniform(-6, 6, 1)
+    for i in range(simlength):
+        new_x = person.x[-1] + person.vx
+        new_y = person.y[-1] + person.vy
+        new_x, new_y = CheckBoundaryLimits(new_x, new_y)
 
-for i in range(simlength):
-    new_x = person.x[-1] + person.vx
-    new_y = person.y[-1] + person.vy
-    new_x, new_y = CheckBoundaryLimits(new_x, new_y)
+        person.x = np.append(person.x, new_x)
+        person.y = np.append(person.y, new_y)
+    people.append(person)
 
-    person.x = np.append(person.x, new_x)
-    person.y = np.append(person.y, new_y)
-
-
+reddots = []
 x_data = []
 y_data = []
 
 fig, ax = plt.subplots()
-ax.set_xlim(-100, 100)
-ax.set_ylim(-100, 100)
-redDot, = ax.plot(person.x[0], person.y[0], "ro")
+ax.set_xlim(-Boundary.x, Boundary.x)
+ax.set_ylim(-Boundary.y, Boundary.y)
+for k in range(num_people):
+    redDot, = ax.plot(people[k].x[0], people[k].y[0], "bo", markersize=20)
+    reddots.append(redDot)
 
-simlength = len(person.y)
-print(len(person.y))
-print(len(person.x))
 
-def animation_frame(i):
-    redDot.set_xdata(person.x[i])
-    redDot.set_ydata(person.y[i])
-    return redDot
+def animation_frame(frame):
+    for d in range(num_people):
+        reddots[d].set_xdata(people[d].x[frame])
+        reddots[d].set_ydata(people[d].y[frame])
+    return reddots
 
 
 animation = FuncAnimation(fig, func=animation_frame, frames=np.arange(0, simlength, 1), interval=10)
